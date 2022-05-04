d7220383ad92e8eb33c434680f51fa58d8d0cc03
Daniel Thulin <daniel_thulin@outlook.com>
2022-05-04 11:11:00 +0200

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import * as React from "react"
 import './App.css';
+import {Routes, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element = {<Home/>}/>
+        <Route path="about" element = {<About/>}/>
+        <Route path="/privacy" element = {<Privacy/>}/>
+      </Routes>
     </div>
   );
+}
+function Home(){
+  return(
+    <>
+    <nav>
+      <ul className='my-list'>
+        <li>Home</li>
+        <li><Link to = "/about">About</Link></li>
+        <li><Link to = "/privacy">Privacy</Link></li>
+      </ul>
+    </nav>
+    <main>
+      <h2>Welcome to home page</h2>
+      <p>You will get more information later</p>
+    </main>
+    </>
+  )
+}
+function About(){
+  return(
+    <>
+    <nav>
+      <ul className='my-list'>
+        <li><Link to = "/home">Home</Link></li>
+        <li>About</li>
+        <li><Link to = "/privacy">Privacy</Link></li>
+      </ul>
+    </nav>
+    <main>
+      <h2>Welcome to about page</h2>
+      <p>I am the king of kings you know</p>
+      <p>North london Forever, what ever the weather, 
+        the streets are our own</p>
+    </main>
+    </>
+  )
+}
+function Privacy(){
+  return(
+    <>
+    <nav>
+      <ul className='my-list'>
+        <li><Link to = "/home">Home</Link></li>
+        <li><Link to = "/about">About</Link></li>
+        <li>Privacy</li>
+      </ul>
+    </nav>
+    <main>
+      <h2>Welcome to privacy page</h2>
+      <p>Yoy will get more information later</p>
+    </main>
+    </>
+  )
 }
 
 export default App;
